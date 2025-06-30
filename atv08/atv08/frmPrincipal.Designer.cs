ab8143301aa4d9a5e00d2dc123cb83ae57e6bf51
Murilo Leandro <muriloleandro188@gmail.com>
2025-06-30 17:02:03 -0300

    .

---
+++ atv08/atv08/frmPrincipal.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace atv08
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.bntConv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digite o Valor em Milhas:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(37, 95);
+            this.txtNum1.Multiline = true;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(156, 44);
+            this.txtNum1.TabIndex = 3;
+            this.txtNum1.Text = "0";
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtNum1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNum1_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Conversão em KM:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(37, 284);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(228, 44);
+            this.txtResultado.TabIndex = 5;
+            this.txtResultado.Text = "0";
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // bntConv
+            // 
+            this.bntConv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntConv.Location = new System.Drawing.Point(37, 179);
+            this.bntConv.Name = "bntConv";
+            this.bntConv.Size = new System.Drawing.Size(86, 44);
+            this.bntConv.TabIndex = 7;
+            this.bntConv.Text = "Converter";
+            this.bntConv.UseVisualStyleBackColor = true;
+            this.bntConv.Click += new System.EventHandler(this.bntConv_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(483, 445);
+            this.Controls.Add(this.bntConv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "frmPrincipal";
+            this.Text = "Milhas para KM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button bntConv;
+    }
+}
+
---
+++ atv08/atv08/frmPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atv08
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void bntConverter_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void bntConv_Click(object sender, EventArgs e)
+        {
+            decimal n1 = Convert.ToDecimal(txtNum1.Text);
+            decimal milhas = 0.6214m;
+            decimal resultado = n1 * milhas;
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void txtNum1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            { 
+                bntConv.Focus();
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
